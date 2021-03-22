c6db888ecbe1d0afd42eb716783abe84f8cde196
aegiuss <rotten.swamp@mail.ru>
2021-03-23 00:31:55 +0300

    GOTY 2021

--- main.cpp
+++ main.cpp
@@ -2,19 +2,52 @@
 
 int main()
 {
-	char ch = 'X';
+	int j;
+	int x = 1, y = 0;
 	initscr();
+	keypad(stdscr, TRUE);
+	raw();
+	cbreak();
+	noecho();
 	while (1)
 	{
-		mvprintw(0, 0, " %c  | %c  |  %c \n", ch, ch, ch);
+		mvprintw(0, 0, "    |    |   \n");
 		printw("--------------\n");
-		mvprintw(2, 0, " %c  | %c  |  %c \n", ch, ch, ch);
+		mvprintw(2, 0, "    |    |   \n");
 		printw("--------------\n");
-		mvprintw(4, 0, " %c  | %c  |  %c \n", ch, ch, ch);
-		mvprintw(6, 0, "Press any key: ");
-		ch = getch();
+		mvprintw(4, 0, "    |    |   \n");
+		mvprintw(6, 0, "Press ENTER to choose cell: ");
+		mvprintw(7, 0, "Press q to exit: ");
+
+		move(y, x);
 		refresh();
+
+		while ((j = getch()) != 'q')
+		{
+			switch (j)
+			{
+				case KEY_LEFT:
+					if (x != 1) x-=5;
+					break;
+				case KEY_RIGHT:
+					if (x != 11) x+=5;
+					break;
+				case KEY_UP:
+					if (y != 0) y-=2;
+					break;
+				case KEY_DOWN:
+					if (y != 4) y+=2;
+					break;
+				case '\n':
+					addch('x');
+					break;
+			}
+			move(y, x);
+			refresh();
+		}
+		break;
 	}
+	endwin();
 	return 0;
 }
 
